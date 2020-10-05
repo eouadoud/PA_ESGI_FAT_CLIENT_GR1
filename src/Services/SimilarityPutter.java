f4733bba0eab10a3099ef8170aff5d2894082ac6
eouadoud <ebr.ouadoud@gmail.com>
2020-10-05 19:42:38 +0200

    delete requet pour les musiqiues dupliqués

--- src/Services/SimilarityPutter.java
+++ src/Services/SimilarityPutter.java
@@ -1,8 +1,10 @@
 package Services;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SimilarityPutter {
@@ -38,6 +40,22 @@ public class SimilarityPutter {
                     + "\u001B[0m");
         }else {
             System.out.println("Message :" + connexion.getResponseMessage());
+        }
+    }
+    public void askToDelete(String adress, int musicId, String token) throws IOException {
+        System.out.println("deleting music with id : "+ musicId);
+        URL url = new URL(adress+musicId);
+        HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
+        connexion.setRequestMethod("DELETE");
+        connexion.setConnectTimeout(1000);
+        connexion.setReadTimeout(1000);
+        connexion.setRequestProperty("Authorization","Bearer " + token );
+        if(connexion.getResponseCode() == 200){
+            System.out.println( "\u001B[32m" + " -------------------- SERVER :: file has been deleted with success --------------------"
+                    + "\u001B[0m");
+        }else{
+            System.out.println(connexion.getResponseMessage() + connexion.getResponseCode() + " ---- " +
+                    connexion.getContent());
         }
     }
 }
